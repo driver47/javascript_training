4c7b840ecc8f757a99763d455b4fdc0759cc7ab8
Daniel <dcaeben@gmail.com>
2021-01-09 15:11:05 -0500

    ES6 Objeto Set

--- ES6-Set/set.js
+++ ES6-Set/set.js
@@ -1,6 +1,7 @@
 /*
-SET es un onjeto que permite almacenar valores unicos de cualquier tipo, incluidos valores primitivos 
-Para considerar dos objetos iguiales debe cumplirse la igualdad estricta (===) con la excepcion 0+ y 0+  Nan+ y Nan-
+LO IMPORTANTE DEL SET ES QUE SOLO TRABAJA CON VALORES UNICOS 
+SET es un objeto que permite almacenar valores unicos de cualquier tipo, incluidos valores primitivos, referencias a objetos etc
+Para considerar dos objetos iguiales debe cumplirse la igualdad estricta (===) con la excepcion 0+ y 0-  Nan+ y Nan-
 */
 
 //SINTAXIS: new Set ([iterable])
@@ -30,7 +31,19 @@ console.log(`¿Está el elemento 2? ${numeros.has(2)}`);
 
 ciudades.add("Barranquilla"); //Nueva
 ciudades.add("Popayan"); // Repetida
-console.log(`El número de ciudades NO REPETIDAS es: ${ciudades.size}`); 
+console.log(`El número de ciudades NO REPETIDAS es: ${ciudades.size}`);
+
+console.log(`El numero de elementos de mezcla NO REPETIDOS es: ${mezcla.size}`);
+let codigos = {28:"Pasto", 30:"Ipiales"};
+mezcla.add(codigos); //lo ingresa como un objeto
+mezcla.add({28:"Pasto", 30:"Ipiales"}); // es igual al objeto en codigos pero lo almacena como uno diferente
+console.log(`El numero de elementos de mezcla NO REPETIDOS es: ${mezcla.size}`);
+
+
+//clear: elimina todos los elementos de un objeto Set
+
+mezcla.clear(); 
+console.log(`El numero de elementos de mezcla NO REPETIDOS es: ${mezcla.size}`);
 
 
 
